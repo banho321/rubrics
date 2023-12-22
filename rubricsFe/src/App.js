89db8a64377b242919aaf0366968793fccc47999
Ba Nguyễn <Ba.nt@inn.edu.vn>
2023-12-22 11:42:04 +0700

    first commit

---
+++ rubricsFe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+<>
+ <h1 className="text-3xl font-bold underline from-teal-400" >
+      Hello world! dsd
+    </h1></>
+  );
+}
+
+export default App;
